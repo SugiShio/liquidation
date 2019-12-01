8210ae9eed242c02f200a11794bfa3c1d3ba0517
Shoko Sugito <fruitmachine0@gmail.com>
2019-12-01 16:36:08 +0900

    pug,firebase使う

---
+++ plugins/firebase.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase'
+
+if (!firebase.apps.length) {
+  firebase.initializeApp({
+    apiKey: 'AIzaSyAYSG3RjbdhqiQyE0sNnKUUigmryi_z07Y',
+    authDomain: 'liquieation.firebaseapp.com',
+    databaseURL: 'https://liquieation.firebaseio.com',
+    projectId: 'liquieation',
+    storageBucket: 'liquieation.appspot.com',
+    messagingSenderId: '45004106445',
+    appId: '1:45004106445:web:0e7c99a9ba4407597b9784'
+  })
+}
+
+export default firebase
